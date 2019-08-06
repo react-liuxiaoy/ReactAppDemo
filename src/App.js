b01e1ca72d9a5fd68fcb91362b12a1d2eba3270b
liuxy <938651469@qq.com>
2019-08-06 17:12:43 +0800

    add f7

--- src/App.js
+++ src/App.js
@@ -1,21 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { App, Navbar, Page, View, Statusbar, PhotoBrowser, Block, Row, Col, Button } from 'framework7-react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class A extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            photos: [
+                {
+                    url: 'img/beach.jpg',
+                    caption: 'Amazing beach in Goa, India',
+                },
+                'http://placekitten.com/1024/1024',
+                'img/lock.jpg',
+                {
+                    url: 'img/monkey.jpg',
+                    caption: 'I met this monkey in Chinese mountains',
+                },
+                {
+                    url: 'img/mountains.jpg',
+                    caption: 'Beautiful mountains in Zhangjiajie, China',
+                },
+            ],
+        }
+    }
+    render() {
+        return (
+            <Page>
+                <Navbar title="Photo Browser" backLink="Back"/>
+                <Block>
+                    <p>Photo Browser is a standalone and highly configurable component that allows to open window with photo viewer and navigation elements with the following features:</p>
+                    <ul>
+                        <li>Swiper between photos</li>
+                        <li>Multi-gestures support for zooming</li>
+                        <li>Toggle zoom by double tap on photo</li>
+                        <li>Single click on photo to toggle Exposition mode</li>
+                    </ul>
+                </Block>
+                <Block>
+                    <p>Photo Browser could be opened in a three ways - as a Standalone component (Popup modification), in Popup, and as separate Page:</p>
+                    <Row>
+                        <Col>
+                            <PhotoBrowser photos={this.state.photos} ref={(el) => {this.standalone = el}} />
+                            <Button raised onClick={() => this.standalone.open()}>Standalone</Button>
+                        </Col>
+                        <Col>
+                            <PhotoBrowser photos={this.state.photos} type="popup" ref={(el) => {this.popup = el}} />
+                            <Button raised onClick={() => this.popup.open()}>Popup</Button>
+                        </Col>
+                        <Col>
+                            <PhotoBrowser photos={this.state.photos} type="page" backLinkText="Back" ref={(el) => {this.page = el}} />
+                            <Button raised onClick={() => this.page.open()}>Page</Button>
+                        </Col>
+                    </Row>
+                </Block>
+                <Block>
+                    <p>Photo Browser suppots 2 default themes - default Light (like in previous examples) and Dark theme. Here is a Dark theme examples:</p>
+                    <Row>
+                        <Col>
+                            <PhotoBrowser photos={this.state.photos} theme="dark" ref={(el) => {this.standaloneDark = el}} />
+                            <Button raised onClick={() => this.standaloneDark.open()}>Standalone</Button>
+                        </Col>
+                        <Col>
+                            <PhotoBrowser photos={this.state.photos} theme="dark" type="popup" ref={(el) => {this.popupDark = el}} />
+                            <Button raised onClick={() => this.popupDark.open()}>Popup</Button>
+                        </Col>
+                        <Col>
+                            <PhotoBrowser photos={this.state.photos} theme="dark" type="page" backLinkText="Back" ref={(el) => {this.pageDark = el}} />
+                            <Button raised onClick={() => this.pageDark.open()}>Page</Button>
+                        </Col>
+                    </Row>
+                </Block>
+            </Page>
+        );
+    }
 }
 
-export default App;
+export default class extends React.Component {
+    render() {
+        return (
+            <App params={{ routes: [{
+                    path: '/',
+                    component: A,
+                }] }}>
+                <Statusbar />
+                <View url="/" main className="ios-edges"/>
+            </App>
+        );
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Framework7 from 'framework7/framework7.esm.bundle';
+import Framework7React from 'framework7-react';
+import 'framework7/css/framework7.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+Framework7.use(Framework7React);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
